1f6b83a865d3968e9ddd3b59e2bdb7ff802cb55f
cxhiano <cxhiano@gmail.comm>
2014-03-26 19:18:22 +0800

    use templates in html instead of generate using pyton

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import os
 
 app = flask.Flask(__name__)
 
-def _gen_url(folder, files):
-    return ['{0}/{1}'.format(folder, fname) for fname in files]
+@app.route('/')
+def display():
+    inputs = [{
+        'id': 'rows',
+        'default': 5,
+        }, {
+        'id': 'columns',
+        'default': 5,
+        }, {
+        'id': 'depotRows',
+        'default': 5,
+        }, {
+        'id': 'depotColumns',
+        'default': 5,
+        }, {
+        'id': 'maxStacks',
+        'default': 6,
+        }]
 
-def _gen_html(temp, items):
-    return flask.Markup('\n'.join([temp.format(item) for item in items]))
-
-def _scripts():
-    html = '<script src="static/{0}"> </script>'
-    js = _gen_url('Flat-UI/js', [
+    scripts_libs = [
             'jquery-1.8.3.min.js',
             'jquery-ui-1.10.3.custom.min.js',
             'jquery.ui.touch-punch.min.js',
@@ -22,36 +33,21 @@ def _scripts():
             'flatui-radio.js',
             'jquery.tagsinput.js',
             'jquery.placeholder.js']
-        )
 
-    js += _gen_url('scripts', ['CascadeView.js', 'view.js', 'lift.js', 'depot.js', 'data.js', 'main.js'])
+    custom_scripts = [
+            'CascadeView.js',
+            'view.js',
+            'lift.js',
+            'depot.js',
+            'data.js',
+            'main.js']
 
-    return _gen_html(html, js)
-
-def _styles():
-    html = '<link href="static/Flat-UI/{0}" rel="stylesheet" />'
-    flat_ui = ['bootstrap/css/bootstrap.css', 'css/flat-ui.css']
-
-    return _gen_html(html, flat_ui)
-
-def _inputs():
-    temp = '''
-            <div class="form-group">
-                <div class="input-group">
-                    <span class="input-group-addon">{0}</span>
-                    <input id="{0}" type="text" class="form-control" placeholder="{0}" value=6 />
-                </div>
-            </div>
-            '''
-    return _gen_html(temp, ['rows', 'columns', 'depotRows', 'depotColumns', 'maxStacks'])
-
-@app.route('/')
-def display():
     args = {
-            'title': 'Title',
-            'styles': _styles(),
-            'inputs': _inputs(),
-            'scripts': _scripts(),
+            'title': 'Sim Port',
+            'styles': ['bootstrap/css/bootstrap.css', 'css/flat-ui.css'],
+            'inputs': inputs,
+            'scripts_libs': scripts_libs,
+            'custom_scripts': custom_scripts,
         }
 
     return flask.render_template('display.html', **args)
