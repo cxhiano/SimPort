6f3582da7aced553c4873356e6c677b59e039e62
cxhiano <cxhiano@gmail.comm>
2014-03-28 15:26:20 +0800

    move to tornado

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
-import flask
-import os
+import os.path
+import tornado.ioloop
+import tornado.web
 
-app = flask.Flask(__name__)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        return self.render('display.html', title = 'Sim Port')
 
-@app.route('/')
-def display():
-    inputs = [{
-        'id': 'rows',
-        'default': 5,
-        }, {
-        'id': 'columns',
-        'default': 5,
-        }, {
-        'id': 'depotRows',
-        'default': 5,
-        }, {
-        'id': 'depotColumns',
-        'default': 5,
-        }, {
-        'id': 'maxStacks',
-        'default': 6,
-        }]
-
-    scripts_libs = [
-            'jquery-1.8.3.min.js',
-            'jquery-ui-1.10.3.custom.min.js',
-            'jquery.ui.touch-punch.min.js',
-            'bootstrap.min.js',
-            'bootstrap-select.js',
-            'bootstrap-switch.js',
-            'flatui-checkbox.js',
-            'flatui-radio.js',
-            'jquery.tagsinput.js',
-            'jquery.placeholder.js']
-
-    custom_scripts = [
-            'CascadeView.js',
-            'view.js',
-            'lift.js',
-            'depot.js',
-            'data.js',
-            'main.js']
-
-    args = {
-            'title': 'Sim Port',
-            'styles': ['bootstrap/css/bootstrap.css', 'css/flat-ui.css'],
-            'inputs': inputs,
-            'scripts_libs': scripts_libs,
-            'custom_scripts': custom_scripts,
-        }
-
-    return flask.render_template('display.html', **args)
-
-@app.route('/apis/test')
-def test():
-    return 'adfdafsadfdsaf'
+def main():
+    app = tornado.web.Application(
+            [
+                (r'/', MainHandler),
+                ],
+            template_path = os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path = os.path.join(os.path.dirname(__file__), 'static'),
+            )
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    main()
